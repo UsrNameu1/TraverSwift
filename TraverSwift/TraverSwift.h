a2c52df91822c702f1124dce83ca67f16e1d7cbb
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2014-12-17 01:16:34 +0900

    Initial Commit

---
+++ TraverSwift/TraverSwift.h
@@ -0,0 +1,19 @@
+//
+//  TraverSwift.h
+//  TraverSwift
+//
+//  Created by adachi yuichi on 2014/12/17.
+//  Copyright (c) 2014年 yad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TraverSwift.
+FOUNDATION_EXPORT double TraverSwiftVersionNumber;
+
+//! Project version string for TraverSwift.
+FOUNDATION_EXPORT const unsigned char TraverSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TraverSwift/PublicHeader.h>
+
+
